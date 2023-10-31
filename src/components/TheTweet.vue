22f2bda613d0e441c528294cd8a35719faab49c2
Yurii Rybachok <drummer2009@ukr.net>
2023-10-31 19:04:32 +0200

    display user data

--- src/components/TheTweet.vue
+++ src/components/TheTweet.vue
@@ -4,6 +4,12 @@
             <figure class="w-16 mr-3">
                 <img :src="tweet.author.profile_img_url" alt="Image" class="rounded-full w-full" />
             </figure>
+            <section class="tweet-body flex-1">
+                <div class="tweet-author">
+                    <span class="text-white font-bold">{{ tweet.author.display_name }}</span>
+                    <span class="text-gray-500 ml-2 font-normal">{{ tweet.author.username }}</span>
+                </div>
+            </section>
         </router-link>
     </article>
 </template>
