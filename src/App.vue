ff124c68c45f0cd9817bc94ee61e982832916fec
Yurii Rybachok <drummer2009@ukr.net>
2023-10-31 10:12:03 +0200

    add styling for header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <header>
-      <router-link to="/profile">
+    <header class="flex items-center border-b border-gray-600">
+      <router-link to="/profile" class="p-4 mr-3 text-green-500 text-2xl">
         <i class="fas fa-user"></i>
       </router-link>
+      <h1 class="text-white font-black text-xl">{{ $route.name }}</h1>
     </header>
     <main>
       <router-view />
