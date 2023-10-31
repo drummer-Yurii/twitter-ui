c97b3f071a3c2c07b9eda74319d40424bd28bc30
Yurii Rybachok <drummer2009@ukr.net>
2023-10-31 19:11:05 +0200

    display tweets of users

--- src/components/TheTweet.vue
+++ src/components/TheTweet.vue
@@ -4,10 +4,14 @@
             <figure class="w-16 mr-3">
                 <img :src="tweet.author.profile_img_url" alt="Image" class="rounded-full w-full" />
             </figure>
-            <section class="tweet-body flex-1">
-                <div class="tweet-author">
+            <section class="flex-1">
+                <div>
                     <span class="text-white font-bold">{{ tweet.author.display_name }}</span>
                     <span class="text-gray-500 ml-2 font-normal">{{ tweet.author.username }}</span>
+                </div>
+
+                <div :class="(tweet.image ? 'mb-3' : '')">
+                    <p class="text-white" v-html="tweet.body"></p>
                 </div>
             </section>
         </router-link>
